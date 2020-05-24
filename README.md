c9b47d425c7b59a240ccd154679950e5309ec42f
LennartMart <lennerdie@gmail.com>
2020-05-24 17:23:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# OneSignal Joomla Plugin
+
+This plugin will send notifications when a new article is published.
+Powered by OneSignal and Joomla!
+
+## Installation
+
+Install the zip file in 'Releases'
+
+## Documentation
+
+### OneSignal App Id
+
+### OneSignal REST Key
+
+1. Go to the dashboard of your app: https://app.onesignal.com
+2. Click on 'Settings'
+3. Click on 'Keys & IDs'
+
+### Select a category
+
+You can select one or more categories. The plugin will only send notifications when an article is published in these categories
+
+If empty, all categories are included
+
+### Only featured articles?
+
+(Default: Yes)
+If true, only featured (or 'special') articles will generate a notification
+
+### Message title
+
+The title of the notification message. If empty: 'New article'.
+The content of the message will contain the title of the article.
+
+### Language
+
+Language of the notification message. Default 'en'.
+
+### Segments
+
+The targeted users. If empty: 'Subscribed Users'
---
+++ onesignalplugin.php
@@ -0,0 +1,65 @@
+<?php
+
+/**
+ * @package     OneSignal Plugin
+ *
+ * @copyright   Copyright (C) 2020. All rights reserved
+ * @license     MIT License; see LICENSE
+ */
+
+defined('_JEXEC') or die;
+
+/**
+ * Plugin push notifications to OneSignal
+ */
+class plgContentOneSignalPlugin extends JPlugin
+{
+    public function onContentAfterSave($context, $article, $isNew)
+    {
+        // Only when creating a new article
+        if (isset($context) && $context == "com_content.article" && $isNew) {
+            $categories = $this->params->get('categories', '');
+            $featured = $this->params->get('featured', 1);
+            if ($article->featured >= $featured && ($categories == '' || (isset($article->catid) && in_array($article->catid, $categories)))) {
+                $this->sendPushNotification($article->title, $this->getLinkToArticle($article));
+            }
+        }
+    }
+    private function sendPushNotification($article_title, $article_link)
+    {
+        // Get the settings
+        $onesignal_app_id = $this->params->get('oneSignalAppId', '');
+        $onesignal_rest_key = $this->params->get('oneSignalRestKey', '');
+        $message_title = $this->params->get('messageTitle', 'New article');
+        $segments = $this->params->get('segments', 'Subscribed Users');
+        $language = $this->params->get('language', 'en');
+
+        // API endpoint
+        $url = 'https://onesignal.com/api/v1/notifications';
+
+        // Header with basic authentication
+        $header = array("Content-Type: application/json; charset=utf-8", 'Authorization: Basic ' . $onesignal_rest_key);
+        // Notification's heading & content 
+        $headings = array($language => $message_title);
+        $contents = array($language => $article_title);
+        // Segments
+        $segments = explode(',', $segments);
+
+        // HTTP request data
+        $data = array('app_id' => $onesignal_app_id, 'headings' => $headings, 'contents' => $contents, 'included_segments' => $segments, 'url' => $article_link);
+        $options = array(
+            'http' => array(
+                'header'  => $header,
+                'method'  => 'POST',
+                'content' => json_encode($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        return $result;
+    }
+    private function getLinkToArticle($article)
+    {
+        return rtrim(JUri::root(), '/') . '/index.php?option=com_content&view=article&id=' . $article->id;
+    }
+}
